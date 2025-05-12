1fdc4943b0a2ec7d1429482ea0b0fbb945bd6db3
Pat Prochacki <patlen123@hotmail.com>
2025-05-12 17:02:17 +0200

    Updated translations

--- translations.js
+++ translations.js
@@ -19,5 +19,10 @@ const TRANSLATIONS = {
   "Turn Off Microphone": "Analluogi Meicroffon",
   "Add a Server": "Ychwanegu Gweinydd",
   "About Me": "Amdanaf",
-  "Member Since": "Aelod Ers"
+  "Member Since": "Aelod Ers",
+  
+  "It's quiet for now...": "Mae hi'n dawel am y tro...",
+  "When a friend starts an activity – like playing a game or hanging out on voice – we'll show it here!": "Pan fydd ffrind yn dechrau gweithgaredd – fel chwarae gêm neu siarad mewn sgwrs llais – byddwn ni'n ei ddangos yma!",
+
+  "Where would you like to go?": "Lle hoffech chi fynd?"
 };
